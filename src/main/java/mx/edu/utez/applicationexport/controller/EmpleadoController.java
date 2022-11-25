a2a9d958ccaecf78b86aa1314e9e0179911de374
Ricardo Rodriguez Gonzalez <20203tn145@utez.edu.mx>
2022-11-25 14:36:47 -0600

    Se añadieron funcionalidades CRUD

--- src/main/java/mx/edu/utez/applicationexport/controller/EmpleadoController.java
+++ src/main/java/mx/edu/utez/applicationexport/controller/EmpleadoController.java
@@ -9,14 +9,86 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class EmpleadoController {
 
     @Autowired
     private EmpleadoServicio empleadoServicio;
+
+    @GetMapping("/ver/{id}")
+    public String verDetallesEmpleado(@PathVariable(value = "id") Long id, Map<String, Object> modelo, RedirectAttributes flash){
+        Empleado empleado = empleadoServicio.findOne(id);
+        if (empleado == null) {
+            flash.addFlashAttribute("error", "El empleado no existe en la base de datos");
+            return "redirect:/listar";
+        }
+        modelo.put("empleado", empleado);
+        modelo.put("titulo", "Detalles del empleado: " + empleado.getNombre());
+        return "Ver";
+    }
+
+    @GetMapping("/form")
+    public String mostrarFormularioRegistro(Map<String,Object> model) {
+        Empleado empleado = new Empleado();
+        model.put("empleado", empleado);
+        model.put("titulo", "Registro de empleados");
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String guardarEmpleado(@Valid Empleado empleado, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status){
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Registro de empleados");
+            return "form";
+        }
+        String mensaje = (empleado.getId() != null) ? "El empleado ha sido modificado correctamente" : "El empleado ha sido registrado correctamente";
+        empleadoServicio.save(empleado);
+        status.setComplete();
+        flash.addFlashAttribute("succes", mensaje);
+        return "redirect:listar";
+    }
+
+    @GetMapping("/form/{id}")
+    public String editarEmpleado(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+        Empleado empleado = null;
+        if (id > 0) {
+            empleado = empleadoServicio.findOne(id);
+            if (empleado == null) {
+                flash.addFlashAttribute("error", "El empleado no existe en la base de datos");
+                return "redirect:/listar";
+            }
+        }
+        else {
+            flash.addFlashAttribute("error", "El ID del empleado no puede ser cero");
+            return "redirect:/listar";
+        }
+        model.put("empleado", empleado);
+        model.put("titulo", "Edición de empleado");
+        return "form";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String elimanrCliente(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+        if (id > 0) {
+            empleadoServicio.delete(id);
+            flash.addFlashAttribute("success", "El empleado ha sido eliminado correctamente");
+        }
+        return "redirect:/listar";
+    }
+
+
 
     @GetMapping({"/","/listar",""})
     public String listarEmpleados(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
